37b116ac672cf963a00ade4d22d3dff140b7a78e
panditdhamdhere <dhamdherepandit@gmail.com>
2024-02-05 18:37:01 +0530

    toast added

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -6,6 +6,7 @@ import { api } from "@/convex/_generated/api";
 
 import { useOrganization } from "@clerk/nextjs";
 import { useApiMutation } from "@/hooks/use-api-mutation";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
   const { mutate, pending } = useApiMutation(api.board.create);
@@ -16,7 +17,13 @@ export const EmptyBoards = () => {
     mutate({
       orgId: organization.id,
       title: "Untitled",
-    });
+    })
+      .then((id) => {
+        toast.success("Board created!");
+
+        // todo -> redirect to board/id
+      })
+      .catch(() => toast.error("Failed to create board!"));
   };
   return (
     <div className="h-full flex flex-col items-center justify-center">
