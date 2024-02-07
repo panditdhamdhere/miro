ced9239e07bf26fba8ba9a66d8488a6121dcf91c
panditdhamdhere <dhamdherepandit@gmail.com>
2024-02-08 01:46:20 +0530

    started working on footer

---
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -0,0 +1,26 @@
+import { Star } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface FooterProps {
+  title: string;
+  authorLabel: string;
+  createdAtLabel: string;
+  isFavorite: boolean;
+  onClick: () => void;
+  disabled: boolean;
+}
+
+export const Footer = ({
+  title,
+  authorLabel,
+  createdAtLabel,
+  isFavorite,
+  onClick,
+  disabled,
+}: FooterProps) => {
+    return (
+        <div>
+            Footer
+        </div>
+    )
+}
--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Overlay } from "./overlay";
+import { formatDistanceToNow } from "date-fns";
+import { useAuth } from "@clerk/nextjs";
+import { Footer } from "./footer";
 
 interface BoardCardProps {
   id: string;
@@ -23,6 +26,9 @@ export const BoardCard = ({
   orgId,
   isFavorite,
 }: BoardCardProps) => {
+  const { userId } = useAuth();
+  const authorLabel = userId === authorId ? "You" : authorName;
+  const createdAtLabel = formatDistanceToNow(createdAt, { addSuffix: true });
   return (
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
@@ -30,6 +36,15 @@ export const BoardCard = ({
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
         </div>
+
+        <Footer
+          isFavorite={isFavorite}
+          title={title}
+          authorLabel={authorLabel}
+          createdAtLabel={createdAtLabel}
+          onClick={() => {}}
+          disabled={false}
+        />
       </div>
     </Link>
   );
