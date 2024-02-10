92c9a56fa1fbdc2b41b2344854cca0ecf09f7568
panditdhamdhere <dhamdherepandit@gmail.com>
2024-02-10 07:41:07 +0530

    copy board link function

--- components/actions.tsx
+++ components/actions.tsx
@@ -16,6 +16,8 @@ import {
   DropdownMenuTrigger,
   DropdownMenuSeparator,
 } from "./ui/dropdown-menu";
+import { Link2 } from "lucide-react";
+import { toast } from "sonner";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -32,10 +34,27 @@ export const Actions = ({
   id,
   title,
 }: ActionsProps) => {
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link copied"))
+      .catch(() => toast.error("Failed to copy link"));
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
-      
+      <DropdownMenuContent
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
