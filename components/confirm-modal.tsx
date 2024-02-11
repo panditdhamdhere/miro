ccdd79f4f272400bb5cba9ca200daf673a699520
panditdhamdhere <dhamdherepandit@gmail.com>
2024-02-11 08:14:36 +0530

    confirm modal

---
+++ components/confirm-modal.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from "./ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  disabled: boolean;
+  header: string;
+  description?: string;
+  onConfirm: () => void;
+}
+
+export const ConfirmModal = ({
+  children,
+  disabled,
+  header,
+  description,
+  onConfirm,
+}: ConfirmModalProps) => {
+  const handleConfirm = () => {
+    onConfirm();
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{header}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel disabled={disabled} onClick={handleConfirm}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
