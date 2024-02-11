4b048d7395adc7de23214d0b69c7b9eb5d2312d9
panditdhamdhere <dhamdherepandit@gmail.com>
2024-02-11 07:59:21 +0530

    board delete func added

--- components/actions.tsx
+++ components/actions.tsx
@@ -16,8 +16,10 @@ import {
   DropdownMenuTrigger,
   DropdownMenuSeparator,
 } from "./ui/dropdown-menu";
-import { Link2 } from "lucide-react";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -34,11 +36,18 @@ export const Actions = ({
   id,
   title,
 }: ActionsProps) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
   const onCopyLink = () => {
     navigator.clipboard
       .writeText(`${window.location.origin}/board/${id}`)
       .then(() => toast.success("Link copied"))
       .catch(() => toast.error("Failed to copy link"));
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delete board"));
   };
 
   return (
@@ -53,6 +62,11 @@ export const Actions = ({
         <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
           <Link2 className="h-4 w-4 mr-2" />
           Copy board link
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onDelete}>
+          <Trash2 className="h-4 w-4 mr-2" />
+          Delete
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
